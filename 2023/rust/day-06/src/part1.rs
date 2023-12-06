436879dcf2c21b0616d4a20e79ed02adb795cc60
osaton <github@osasto.dev>
2023-12-06 21:43:51 +0200

    Refactor Day 2 part 1

--- 2023/rust/day-06/src/part1.rs
+++ 2023/rust/day-06/src/part1.rs
@@ -28,26 +28,21 @@ impl Race {
 }
 
 #[tracing::instrument]
+fn parse_line(line: Option<&str>) -> Result<Vec<u32>, &str> {
+    line.ok_or("No line found")
+        .and_then(|line| line.split(':').last().ok_or("Missing ':'"))
+        .map(|nums| {
+            nums.split_whitespace()
+                .map(|s| s.parse::<u32>().map_err(|e| "Failed to parse"))
+        })
+        .and_then(|nums| nums.collect::<Result<Vec<u32>, &str>>())
+}
+
+#[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String, AocError> {
     let mut parts = _input.split('\n');
-    let times = parts
-        .next()
-        .unwrap()
-        .split(':')
-        .last()
-        .unwrap()
-        .split_whitespace()
-        .map(|number| number.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
-    let distances = parts
-        .next()
-        .unwrap()
-        .split(':')
-        .last()
-        .unwrap()
-        .split_whitespace()
-        .map(|number| number.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+    let times = parse_line(parts.next()).expect("Failed to parse times");
+    let distances = parse_line(parts.next()).expect("Failed to parse distances");
 
     let res = times
         .into_iter()
